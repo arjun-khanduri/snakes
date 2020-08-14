72d991ffd6327ff5bd586755d3cdd333ed3e9bc4
Arjun Khanduri <arjun99.khanduri@gmail.com>
2020-08-14 09:44:43 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Snakes from './Snake1';
 function App() {
+  const style={
+    margin: `2%`,
+    color:`white`
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="text-center" style={style}><h1>Snake Game</h1></div>
+      <Snakes/>
     </div>
   );
 }
