0124fb16b0f4612b9f8369618ff31e33fca03cf5
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-02-08 23:07:42 +0530

    Improved Formatting

--- src/Food.js
+++ src/Food.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export default(props)=>{
-    const style={
-        left:`${props.dot[0]}%`,
-        top:`${props.dot[1]}%`
+export default (props) => {
+    const style = {
+        left: `${props.dot[0]}%`,
+        top: `${props.dot[1]}%`
     }
-    return(
+    return (
         <div className="snake-food" style={style}></div>
     )
 }
